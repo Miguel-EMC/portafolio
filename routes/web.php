3bf257a597b54c73e794486c22fc9608a32f177c
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-06-01 18:40:52 -0500

    portafolio-final

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PortafolioController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +20,23 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::view('portafolio','portafolio.index')->name('portafolio');
+
+Route::get('portafolio',[PortafolioController::class,'index'])->name('portafolio');
+
+Route::get('portafolio/create',[PortafolioController::class,'create'])->name('create');
+
+Route::post('portafolio',[PortafolioController::class,'store'])->name('store');
+
+Route::get('portafolio/{id}',[PortafolioController::class,'show'])->name('show');
+
+Route::get('portafolio/edit/{id}',[PortafolioController::class,'edit'])->name('edit');
+
+Route::put('portafolio/{portafolio}',[PortafolioController::class,'update'])->name('update');
+
+Route::delete('portafolio/{portafolio}',[PortafolioController::class,'destroy'])->name('destroy');
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
